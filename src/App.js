315d2e8658eb29c95baf35a8f3855bbdfd58327d
KatsuhideAsanuma <katsuhide.asanuma@gmail.com>
2023-02-02 11:14:25 +0900

    master

--- src/App.js
+++ src/App.js
@@ -1,45 +1,44 @@
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 //import { bindActionCreators } from 'redux';
-import {addToDo,removeToDo} from './actions';
-import Container from './ClassContainer';
+import { addToDo, removeToDo } from "./actions";
+import Container from "./Container";
 
-
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App(props) {
-  const [input,setInput]=useState("");
+  const [input, setInput] = useState("");
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          WELCOME to React Redux ToDo List
-        </p>
+        <p>WELCOME to React Redux ToDo List</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-        </a>
+        ></a>
       </header>
       <ul>
-        {props.todos.map(todo=>{
-          return(
-            <li key={todo}><span>{todo}</span><button onClick={()=>props.removeToDo(todo)}>DEL</button></li>
-          )
+        {props.todos.map((todo) => {
+          return (
+            <li key={todo}>
+              <span>{todo}</span>
+              <button onClick={() => props.removeToDo(todo)}>DEL</button>
+            </li>
+          );
         })}
       </ul>
-      <input type="text" onChange={e=>setInput(e.target.value)}/>
-      <button onClick={()=>props.addToDo(input)}>submit</button>
-      <Container/>
+      <input type="text" onChange={(e) => setInput(e.target.value)} />
+      <button onClick={() => props.addToDo(input)}>submit</button>
+      <Container />
     </div>
   );
 }
-const mapStateToProps=state=>{
-  return{
-    todos:state.todos.list
-  }
+const mapStateToProps = (state) => {
+  return {
+    todos: state.todos.list,
+  };
 };
 /*
 const mapDispatchToProps=dispatch=>{
@@ -49,7 +48,7 @@ const mapDispatchToProps=dispatch=>{
   },dispatch)
 }
 */
-export default connect(mapStateToProps,{addToDo,removeToDo})(App);
+export default connect(mapStateToProps, { addToDo, removeToDo })(App);
 
 /*
 const mapDispatchToProps=dispatch=>{
